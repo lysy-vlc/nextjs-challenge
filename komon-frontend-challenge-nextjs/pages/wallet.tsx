be4366279be187a285da011ded9b5e8856de005a
lysy-vlc <kamil.grzaba@gmail.com>
2022-09-28 00:11:32 +0200

    wallet

--- komon-frontend-challenge-nextjs/pages/wallet.tsx
+++ komon-frontend-challenge-nextjs/pages/wallet.tsx
@@ -1,9 +1,50 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
+import styles from '../styles/Wallet.module.scss'
 
 const Wallet: NextPage = () => {
-  return (
-    <div>
+  const [address, setAddress] = useState<string>('')
+  const [chainId, setChainId] = useState<string>('')
 
+  const connectMetamask = async (): Promise<void> => {
+
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+
+      // res[0] for fetching a first wallet
+      try {
+        const address = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        setAddress(address)
+
+      } catch (e) {
+        alert('Could\'t connect to metamask. See console.')
+        console.log(e)
+      }
+
+      try {
+        const chainId = await window.ethereum.request({ method: 'eth_chainId' })
+        setChainId(chainId)
+
+      } catch (e) {
+        alert('Could\'t retrieve chainId. See console.')
+        console.log(e)
+      }
+    } else {
+      alert('install metamask extension!!')
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <button onClick={connectMetamask}>conect</button>
+      <div>
+        <div className={styles['property-wrapper']}>
+          address: <span>{ address }</span>
+        </div>
+        <div className={styles['property-wrapper']}>
+          chainId: <span>{ chainId }</span>
+        </div>
+      </div>
     </div>
   )
 }
